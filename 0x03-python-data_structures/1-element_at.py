9e096464e503a9af46ffa9c57259b1881a8d93d8
Nzemeka Akponor <vakponor@gmail.com>
2022-12-05 14:06:44 -0800

    1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
+
 def element_at(my_list, idx):
+    """Retrieves an element from a list like in C
+
+    Args:
+        my_list: a list
+        idx: the index of item to retrieve
+
+    Returns:
+        the item at index idx
+    """
+
+    # Check for negative and out of range index
     if idx < 0:
-        return (None)
-
-    length = len(my_list)
-
-    if idx > length - 1:
-        return (None)
-
-    return(my_list[idx])
+        return None
+    if idx >= len(my_list):
+        return None
+    return my_list[idx]
