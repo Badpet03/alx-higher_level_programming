1635196b16b197f27c62210d86ec54ff86398bfb
Nzemeka Akponor <vakponor@gmail.com>
2022-12-05 13:36:23 -0800

    13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -26,7 +26,7 @@ void reverse_listint(listint_t **head)
 /**
  * is_palindrome - checks if a linked list is a palindrome
  * @head: double pointer to the linked list
- *
+
  * Return: 1 if it is, 0 if not
  */
 int is_palindrome(listint_t **head)
@@ -70,4 +70,3 @@ int is_palindrome(listint_t **head)
 
 	return (0);
 }
-
