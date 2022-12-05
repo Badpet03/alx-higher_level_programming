cac6ba999de3a3296a0221869afb4f45efdedf3c
Nzemeka Akponor <vakponor@gmail.com>
2022-12-05 06:26:59 -0800

    0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for lst_elem in range(len(my_list)):
-        print("{:d}".format(my_list[lst_elem]))
-
+    for elem in my_list:
+        # if isinstance(elem, int):
+        print("{:d}".format(elem))
